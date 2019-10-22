81a9a31804fb171a0935613c6877433d389884e9
Sung Lee <sungsoolee2@hotmail.com>
2019-10-22 14:00:55 -0500

    change

--- 03-interview-qs/1-Interview-Questions/palindrome/Solutions/direct_solution.js
+++ 03-interview-qs/1-Interview-Questions/palindrome/Solutions/direct_solution.js
@@ -1,5 +1,13 @@
 "use strict";
 
-function isPalindrome(string) {
-  return string.split("").reverse().join("") === string;
-}
+// function isPalindrome(string) {
+//   return string.split("").reverse().join("") === string;
+// }
+
+function isPalindrome(str){
+  return str.split('').reverse().join('') === str;
+}  
+console.log(isPalindrome("kayak"))
+
+
+
